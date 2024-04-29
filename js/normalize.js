ce0c10527a90390473f200725ba07fc21a2b06ba
Ruslan Kushakov <kushakov5@gmail.com>
2024-04-30 01:42:53 +0300

    alg2

--- js/normalize.js
+++ js/normalize.js
@@ -1,3 +1,11 @@
 $(function() {
 	
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+
 });
